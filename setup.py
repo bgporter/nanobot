f1c2580f92433adc113bd0110f055bf443aed3a6
Brett g Porter <bgp@bgporter.net>
2016-06-16 11:51:11 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setuptools
+
+
+# Structure of this lifted from and thanks to 
+# https://pythonhosted.org/an_example_pypi_project/setuptools.html
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Nanobot",
+    version = "0.5.0",
+    author = "Brett g Porter",
+    author_email = "bgp@bgporter.net"
+    description = "A tiny twitterbot framework."
+    license = "MIT",
+    keywords = "twitter twitterbot bot framework",
+    url = "https://github.com/bgporter/nanobot"
+    packages=['nanobot']
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
